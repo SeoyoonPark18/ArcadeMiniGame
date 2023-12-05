95d7840c4cf24bcdb2f19e1083e59f464439f6dd
psg9790 <81291990+psg9790@users.noreply.github.com>
2023-12-05 16:11:55 +0900

    ui

--- Assets/ShootingGame/Scripts/ModeStartUI.cs
+++ Assets/ShootingGame/Scripts/ModeStartUI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class ModeStartUI : MonoBehaviour
@@ -14,7 +15,7 @@ public class ModeStartUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void GoMainmenu()
     {
